f1eeb2bca185d8240c6b87f27f07cb6e2e752207
Ксения Голубева <golubeva3.ka@edu.spbstu.ru>
2021-03-03 20:45:49 +0300

    hello

---
+++ size_of/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace size_of
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(36, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(319, 258);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(436, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(242, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(436, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(242, 26);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(436, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Размер:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(436, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(199, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Емкостная размерность:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ size_of/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace size_of
+{
+    public partial class Form1 : Form
+    {
+        const int n = 5;
+        int[] E = { 1, 5, 7, 35, 101};
+        string path = "image.png";
+        double[] X = new double[n];
+        double[] Y = new double[n];
+        int h;
+        int w;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(path);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.Image = img;
+            h = img.Height;
+            w = img.Width;
+            textBox1.Text = h.ToString() + "*" + w.ToString();
+            textBox2.Text = capacitiveDimension().ToString();
+        }
+
+        private double capacitiveDimension()
+        {
+            int b = 0;
+            Image<Gray, Byte> img = new Image<Gray, Byte>(path);
+            for (int i = 0; i < n; i++) 
+            {
+                X[i] = Math.Log(1.0 / E[i]);
+                Y[i] = Math.Log(Ne(img, E[i]));
+            }
+
+            return LS();
+        }
+
+        private int Ne(Image<Gray, Byte> img, int e)
+        {
+            int res = 0;
+            bool hasBlack;
+            for (int i = 0; i < w; i += e) 
+            {
+                for (int j = 0; j < h; j += e) 
+                {
+                    hasBlack = false;
+                    for (int x = 0; x < e && !hasBlack && i + x < w; x++) 
+                    {
+                        for (int y = 0; y < e && !hasBlack && j + y < h; y++) 
+                        {
+                            if (img.Data[i + x, j + y, 0] < 127) 
+                            {
+                                hasBlack = true;
+                            }
+                        }
+                    }
+                    if (hasBlack)
+                    {
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+        private double LS()
+        {
+            double sumx = 0;
+            double sumy = 0;
+            double sumx2 = 0;
+            double sumxy = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sumx += X[i];
+                sumy += Y[i];
+                sumx2 += X[i] * X[i];
+                sumxy += X[i] * Y[i];
+            }
+            sumx = sumx / n;
+            sumy = sumy / n;
+            sumx2 = sumx2 / n;
+            sumxy = sumxy / n;
+            return (sumxy - sumx * sumy) / (sumx2 - sumx * sumx);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
